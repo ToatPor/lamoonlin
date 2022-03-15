f0448eb38978253a31593b79a841083cb72089b8
ToatPor <88965520+ToatPor@users.noreply.github.com>
2022-03-15 23:22:08 +0700

    shit

--- javascript/sciprt.js
+++ javascript/sciprt.js
@@ -7,9 +7,36 @@ const navigationIcon = document.querySelector(".navigation_icon");
 const checkBox = document.getElementById("checkbox");
 const openClose = document.querySelector(".open_close");
 const allLink = document.querySelectorAll("a:link");
+const link = [...allLink];
 
+//external link
+const externalLink = function (link) {
+  const exLink = link.filter((cur) => {
+    const arr = cur.getAttribute("href").charAt(0) !== "#";
+    return arr;
+  });
+  exLink.forEach((cur) => {
+    cur.addEventListener("click", function (event) {
+      event.preventDefault();
+      const href = cur.getAttribute("href");
+      console.log(href);
+      href === "https://lin.ee/yF4W1vj" &&
+        window.open("https://lin.ee/yF4W1vj") &&
+        checkBox.removeAttribute("checked");
+      href === "tel:0645546362" && window.open("tel:0645546362");
+      href === "mailto:lamoon.linbox@gmail.com" &&
+        window.open("mailto:lamoon.linbox@gmail.com");
+      href === "https://www.facebook.com/lamoon.linbox" &&
+        window.open("https://www.facebook.com/lamoon.linbox");
+    });
+  });
+};
+//internal link
 const toggleChecked = function (arr) {
-  arr.forEach((cur) => {
+  const internalLink = arr.filter((cur) =>
+    cur.getAttribute("href").startsWith("#")
+  );
+  internalLink.forEach((cur) => {
     cur.addEventListener("click", function (e) {
       e.preventDefault();
       const href = cur.getAttribute("href");
@@ -27,36 +54,30 @@ const toggleChecked = function (arr) {
         });
         checkBox.removeAttribute("checked");
       }
-      href === "https://lin.ee/yF4W1vj" &&
-        window.open("https://lin.ee/yF4W1vj") &&
-        checkBox.removeAttribute("checked");
-      href === "tel:0645546362" && window.open("tel:0645546362");
-      href === "mailto:lamoon.linbox@gmail.com" &&
-        window.open("mailto:lamoon.linbox@gmail.com");
-      href === "https://www.facebook.com/lamoon.linbox" &&
-        window.open("https://www.facebook.com/lamoon.linbox");
     });
   });
 };
+//toggle button
+const toggleButton = function (check, nav) {
+  nav.addEventListener("click", function (e) {
+    e.preventDefault();
+    check.toggleAttribute("checked");
+  });
+};
 
-toggleChecked(allLink);
-// for (const link of navigationLink) {
-//   link.preventDefault();
-// }
-
-// for (let i = 0; i < navigationLink.length; i++) {
-//   navigationLink[i].addEventListener("click", function () {
-//     if (checkBox.getAttribute("checked")) {
-//       // navigationBackground.classList.add("hidden");
-//       // navigationList.classList.add("hidden");
-//       navigationIcon.style.transform =
-//         "rotate(90deg)" && checkBox.toggleAttribute("checked");
-
-//       // checkBox.getAttribute("checked") && checkBox.removeAttribute("checked");
-//     }
-//   });
-// }
-navigationButton.addEventListener("click", function (e) {
-  e.preventDefault();
-  checkBox.toggleAttribute("checked");
-});
+const clickBackground = function (background, check) {
+  background.addEventListener("click", function (e) {
+    e.preventDefault();
+    check.removeAttribute("checked");
+  });
+};
+const pressEsc = function (check) {
+  document.addEventListener("keydown", function (event) {
+    event.key === "Escape" && check.removeAttribute("checked");
+  });
+};
+toggleButton(checkBox, navigationButton);
+externalLink(link);
+toggleChecked(link);
+clickBackground(navigationNav, checkBox);
+pressEsc(checkBox);
